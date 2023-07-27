9c4d34a5deb95e72b2e5ad3dbbcb7056524f4613
Sanna Talola <48483040+stalola@users.noreply.github.com>
2023-07-27 16:42:52 +0300

    Create first version

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from sqlalchemy.sql import text
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    db.session.execute(text("INSERT INTO visitors (time) VALUES (NOW())"))
+    db.session.commit()
+    result = db.session.execute(text("SELECT COUNT(*) FROM visitors"))
+    counter = result.fetchone()[0]
+    return render_template("index.html", counter=counter)
